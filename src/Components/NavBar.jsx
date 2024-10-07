73d67b745add99340a2737c336fedc772d2b7592
achoCode <luckyacho2@gmail.com>
2024-10-07 22:50:57 +0100

    Hero section completed

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,10 +1,26 @@
-import React from "react";
-import logo from "../assets/logos/navLogo.png";
-import {Link} from 'react-router-dom'
+import React, { useState, useEffect } from "react";
+import logo from "../assets/logos/navLogobl.png";
+import { Link } from "react-router-dom";
 export const NavBar = () => {
+  const [isFixed, setIsFixed] = useState(false);
+
+  useEffect(() => {
+    const handleIsfixed = () => {
+      if (window.pageYOffset > 100) {
+        setIsFixed(true);
+      } else {
+        setIsFixed(false);
+      }
+    };
+    window.addEventListener("scroll", handleIsfixed);
+
+    return () => {
+      window.removeEventListener("scroll", handleIsfixed);
+    };
+  }, []);
   return (
     <>
-      <div className="nav-bar">
+      <div className={`nav-bar ${isFixed ? `fixed` : ``}`}>
         <img src={logo} className="logo" alt="logo.png" />
 
         <ul>
--- src/Components/Pagecomponents/HomePg.jsx
+++ src/Components/Pagecomponents/HomePg.jsx
@@ -1,25 +1,62 @@
 import React from "react";
 import { NavBar } from "../NavBar";
 import { Button } from "../Button";
+import { delay, motion } from "framer-motion";
+
 export const HomePg = () => {
+  const buttonVariants = {
+    visible: {
+      x: [5, -10, 5, -10, 5, -10, 5, -10, 0],
+      transition: {
+        duration: 0.4,
+        delay: 3.5,
+      },
+    },
+  };
   return (
     <div className="page-container">
       <NavBar />
       <div className="hero-section">
         <div className="hero-text-container">
-          <div className="hero-text-1">
-            <h2>HELLO, NEW FRIEND!</h2>
-          </div>
-          <div className="hero-welcome-text">
-            <h1>Welcome Back</h1>
+          <motion.div
+            initial={{ y: -100, opacity: 0 }}
+            animate={{ y: 0, opacity: 1 }}
+            transition={{ duration: 1.8 }}
+            className="hero-text-1"
+          >
             <h2>
-              to <span>Easty Munchies</span>
+              HELLO, <span> NEW FRIEND!</span>
             </h2>
-            <h3>Quick bites, Big Flavors...</h3>
+          </motion.div>
+          <div className="hero-welcome-text">
+            <motion.h1
+              initial={{ y: 100, opacity: 0 }}
+              animate={{ y: 0, opacity: 1 }}
+              transition={{ duration: 1.8 }}
+            >
+              Welcome Back
+            </motion.h1>
+            <motion.h2
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.8, delay: 1 }}
+            >
+              to <span>Easty Munchies</span>
+            </motion.h2>
+            <motion.h3
+              initial={{ x: 300, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1.8, delay: 1.5 }}
+            >
+              Quick bites, Big Flavors...
+            </motion.h3>
           </div>
         </div>
-        <Button link="/menu" title="Browse Menu" />
+        <motion.div variants={buttonVariants} animate="visible">
+          <Button link="/menu" title="Browse Menu" />
+        </motion.div>
       </div>
+      <div className="box"></div>
     </div>
   );
 };
